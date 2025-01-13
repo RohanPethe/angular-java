6759ac7cf91b368b6c9462caebb9d9c4907276cf
Rohan Pethe <119988356+RohanPethe@users.noreply.github.com>
2025-01-13 15:10:04 +0530

    Update main.tf

--- spring-backend/main.tf
+++ spring-backend/main.tf
@@ -1,7 +1,7 @@
 provider "aws"{
     region = "ap-northeast-2"
     }
-   resources "aws_instance" "web-website"{
+   resource "aws_instance" "web-website"{
    instance_Type = t2.medium
    instanceVolume = 8
    ami ="ami-0dc44556af6f78a7b "
